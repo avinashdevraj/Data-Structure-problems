5bc74c3654a88ce075c7b35d2a60ce5f7af28f78
avinashdevraj <avinashdevraj1234@gmail.com>
2024-02-10 22:32:07 +0530

    Palindromic Substrings

---
+++ Problems/647. Palindromic Substrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    int count;
+    public int countSubstrings(String s) {
+        int n=s.length();
+        count=0;
+        for(int i=0;i<n;i++){
+            int evencount=checkpal(s,i,i+1);
+
+            int oddcount=checkpal(s,i-1,i+1);
+
+            count=count+evencount+oddcount+1;
+        }
+        return count;
+
+    }
+    int checkpal(String s,int i,int j){
+        int count=0;
+        while(i>=0 && j<s.length() && s.charAt(i--)==s.charAt(j++)){
+            count++;
+        }
+        return count;
+    }
+
+}
