cbd37d090c5346708c7dda0ca149d99c3ee13bcb
avinashdevraj <avinashdevraj1234@gmail.com>
2024-02-20 11:38:01 +0530

    Missing Number

---
+++ Problems/268. Missing Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        for(int x:nums){
+            sum+=x;
+        }
+        return n*(n+1)/2-sum;
+
+    }
+}
