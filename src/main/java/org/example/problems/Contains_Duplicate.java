c209b4b2cb34dd15c835a58b8a1663c4380fbb6d
avinashdevraj <avinashdevraj1234@gmail.com>
2024-08-15 20:55:20 +0530

    duplicateele

---
+++ src/main/java/org/example/problems/Contains_Duplicate.java
@@ -0,0 +1,24 @@
+package org.example.problems;
+
+import java.util.HashMap;
+
+public class Contains_Duplicate {
+
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            HashMap<Integer,Integer> map=new HashMap<>();
+            for(int i=0;i<nums.length;i++){
+                if(map.containsKey(nums[i])){
+                    if(Math.abs(i-map.get(nums[i]))<=k) return true;
+                    else{
+                        map.replace(nums[i],i);
+                    }
+                }
+                else{
+                    map.put(nums[i],i);
+                }
+            }
+            return false;
+
+        }
+
+}
